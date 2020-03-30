08f8796615622b63db8606a6220f9b2627dc881f
Alex Goodall <alex.w.goodall@gmail.com>
2020-03-30 18:06:13 +0200

    added android resources

--- changing_lives/changing_lives.Android/MainActivity.cs
+++ changing_lives/changing_lives.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace changing_lives.Droid
 {
-    [Activity(Label = "changing_lives", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Changing-Lives", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
